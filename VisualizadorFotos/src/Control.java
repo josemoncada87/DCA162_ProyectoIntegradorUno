d7fb073c1028e216655e352de3179deeacfdbb0d
Jose Moncada <josemoncada87@hotmail.com>
2016-09-16 08:59:43 -0500

    cambios en el control

--- VisualizadorFotos/src/Control.java
+++ VisualizadorFotos/src/Control.java
@@ -4,6 +4,26 @@ public class Control {
 	/**
 	 * @author Monk testing... 
 	 */
+	
+	
+	public Control() {
+	
+	}
+	
+	@Override
+	public boolean equals(Object arg0) {
+		return super.equals(arg0);
+	}	
+	
+	@Override
+	public int hashCode() {	
+		return super.hashCode();
+	}
+	
+	@Override
+	public String toString() {	
+		return super.toString();
+	}
 	 
 
 }
